2712ced37f39121fc26d21d71bf35365d6d2b866
rameck88 <73326089+rameck88@users.noreply.github.com>
2021-01-28 09:29:55 -0300

    Update acesso.php

--- acoes/acesso.php
+++ acoes/acesso.php
@@ -30,13 +30,13 @@
 
             $query = $conexao->prepare("SELECT * FROM usuarios WHERE email = ? AND senha = ?");
             $query->execute(array($email, $senha));
-           
+           /*("SELECT u.*,pu.* FROM usuarios as u INNER JOIN perfil_user as pu ON p.id = pu.id_user WHERE u.email = ? AND u.senha = ?");*/
             if($query->rowCount()){
                 $user = $query->fetchAll(PDO::FETCH_ASSOC)[0];
-
+                      
                 session_start();
-                $_SESSION["usuario"] = array($user["nome"], $user["nivel"],);
-
+                $_SESSION["usuario"] = array($user["nome"], $user["nivel"], $user["id"],$user["email"]);/*,$user["email"], $user["cpf"], $user["d_nasc"], $user["tel"], $user["profissao"], $user["bairro"], $user["cidade"], $user["endereco"], $user["estado"], $user["numero"], $user["cep"], $userr["complemento"],*/
+           
                 return json_encode(array("erro" => 0));
             }else{
                 return json_encode(array("erro" => 1, "mensagem" => "Email e/ou senha incorretos."));
@@ -49,9 +49,16 @@
             $query = $conexao->prepare("INSERT INTO usuarios (email, senha, nome, nivel) VALUES (?, ?, ?, ?)");
             
             if($query->execute(array($email, $senha, $nome, 0))){
+                /*$id = mysqli_insert_id($conexao);
+                $db2 = $conexao->prepare("INSERT INTO perfil_user (cpf, d_nasc, tel, profissao, id_user) VALUES ('0', '0', '0', '0', '{$id}')");
+                    if($db2->execute()){
+                        $db3 = $conexao->prepare("INSERT INTO perfil_end (bairro, cidade, endereco, estado, numero, cep, complemento, id_perfil ) VALUES ('0', '0', '0', '0', '0', '0', '0', '{$id}')");
+                        if($db3->execute()){
+                        }
+                    }*/
                 session_start();
                 $_SESSION["usuario"] = array($nome, 0);
-                
+               
                 return json_encode(array("erro" => 0));
             }else{
                 return json_encode(array("erro" => 1, "mensagem" => "Ocorreu um erro ao cadastrar usuario."));
