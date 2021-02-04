3bfd140c9064a72bf23c92ee1bb8da638c49bd5c
jagutoli <64437840+jagutoli@users.noreply.github.com>
2021-02-04 12:23:29 -0300

    Create cabecalho.php

---
+++ cabecalho.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style/acesso.css"/>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script type="text/javascript" src="script/jquery.js"></script>
+    <script type="text/javascript" src="script/acesso.js"></script>
+    <title>Dashboard - <?php echo $nome; ?></title>
+</head>
+<body>
+    <div class="w3-bar w3-black">
+        <span class="branding w3-bar-item w3-mobile">EAD WEBSITE</span>
+        <span class="w3-right w3-mobile">
+            <a href="#" class="w3-bar-item w3-button w3-mobile w3-hover-green"> Logo/ Foto do Usuário</a>
+        </span>
+    </div>  
+</body>
